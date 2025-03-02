edcac859682911d3a2ce701280e0e5afda3ff66b
Taregh Naderi <taregh.n@gmail.com>
2025-03-03 02:50:48 +0330

    start use git

---
+++ wagtailSite/wagtailSite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-m)lhm-)9(+b^h1aut54ht=-3#))*^f5e)&cyzb9y+1cl^l9zpi"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
